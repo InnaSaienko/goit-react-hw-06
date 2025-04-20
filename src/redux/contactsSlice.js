d30e8be0ebb046f528ffc3aa02dc449b1c0f48bc
Inna Saienko <inna.saienko.nl@gmail.com>
2025-04-20 07:09:32 +0200

    create template Slice

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,1 +1,16 @@
+import { createSlice} from "@reduxjs/toolkit";
 
+const initialState = {
+    items: [],
+};
+
+const slice = createSlice({
+    name: "contacts",
+    initialState,
+    reducers: {
+        addContact: () => {},
+        deleteContact: () => {},
+    }
+})
+export const { addContacts, deleteContacts } = slice.actions;
+export default slice.reducer;
