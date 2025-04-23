49e9cb633452bcc79597e2a96109391839e4616b
Inna Saienko <inna.saienko.nl@gmail.com>
2025-04-23 16:20:22 +0200

    switch to redux

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import Contact from "../Contact/Contact.jsx";
 import s from "./ContactList.module.css"
+import {useSelector} from "react-redux";
 
-function ContactList({contacts, deleteUser}) {
+const ContactList = () => {
+    const users = useSelector(state => state.contacts.items);
+    const filter = useSelector(state => state.filters.name);
+
+    const getFilteredContacts = (userContacts, filterValue) => {
+        if (filterValue !== "") {
+            return userContacts.filter(
+                (contact) =>
+                    contact.name.toLowerCase().includes(filterValue.toLowerCase()) ||
+                    contact.number.includes(filterValue)
+            );
+        }
+        return userContacts;
+    };
+    const filteredContacts = getFilteredContacts(users, filter);
     return (
         <div className={s.list}>
-            {contacts.map((contact, index) => (
-                <Contact key={index} contact={contact} deleteUser={deleteUser}/>
+            {filteredContacts.map((contact, index) => (
+                <Contact key={index} contact={contact}/>
             ))}
         </div>
     );
