f1fee5947a225433c403191eb270976a4b652c9f
Inna Saienko <inna.saienko.nl@gmail.com>
2025-04-23 09:56:32 +0200

    switch to redux

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import s from "./SearchBox.module.css"
+import {useDispatch} from "react-redux";
+import {changeFilter} from "../../redux/filtersSlice.js";
 
-function SearchBox({ filter, setFilter }) {
+function SearchBox() {
+    const dispatch = useDispatch();
+
     return (
         <div className={s.searchBox}>
             <label className={s.label}>Find contact by name</label>
             <input
                 type="text"
                 placeholder="Search contacts..."
-                value={filter}
-                onChange={(e) => setFilter(e.target.value)}
+                name="filter"
+                onChange={(e) => dispatch(changeFilter(e.target.value))}
                 className={s.input}
             />
         </div>
